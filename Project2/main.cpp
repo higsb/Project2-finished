c763f1fa9a29a00e3efbb68573f784e6ce62f459
higsb <dennis.durant2@csu.fullerton.edu>
2017-03-22 17:40:52 -0700

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Dennis Durant II on 3/22/17.
+//  Copyright © 2017 Dennis Durant II. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
