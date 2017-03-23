d8318c60a45e8cb382d8c7e131cb153f5a25626b
higsb <dennis.durant2@csu.fullerton.edu>
2017-03-23 00:30:22 -0700

    Final
    Completed class file

--- Project2/TextTool.h
+++ Project2/TextTool.h
@@ -15,13 +15,14 @@ class TextTool {
 public:
     // default constructor
     TextTool() {
-        // TO BE COMPLETED
-        
+        n = duplicates = 0;
+        wordlist = new list<string>();
     }
     
     // destructor
     ~TextTool() {
-        // TO BE COMPLETED
+        wordlist->clear();
+     
         
     }
     
@@ -43,51 +44,105 @@ public:
     
     // return the number of words in the linked list
     int totalWords() {
-        return -1; // TO BE COMPLETED
+        return n;
         
     }
     
     // add entry at the back of the linked list
     void addEntryBack(const string& aword) {
         
-        // TO BE COMPLETED
         
+        wordlist->push_back(aword);
+        n++;
     }
     
     // print all words stored in the linked list, separated by a space
     const string listToString() {
-        // TO BE COMPLETED
+        string words = "";
+        list<string>::iterator i;
+        for (i = wordlist->begin(); i != wordlist->end(); i++)
+        {
+            words += *i + " ";
+        }
+        return words;
         
     }
     
     // print duplicated words in the linked list
     void printDuplicates() {
-        // TO BE COMPLETED
-        
+        list<string>::iterator i;
+        list<string>::iterator j;
+        for (i = wordlist->begin(); i != wordlist->end(); ++i)
+        {
+            int count = 0;
+            for (j = i; j != wordlist->end(); ++j)
+            {
+                if (count == 0)
+                {
+                    count = 1;
+                    continue;
+                }
+                string a = *i;
+                string b = *j;
+                if (a.compare(b) == 0)
+                {
+                    cout << *i << " ";
+                }
+                break;
+            }
+        }
     }
     
     // remove duplicated words in the linked
     void removeDuplicates() {
-        // TO BE COMPLETED
-        
+        wordlist->unique();
+        calculateDuplicates();
     }
     
     // determine the total number of duplicated words in the list
     int totalDuplicates() {
-        // TO BE COMPLETED
-        
+        //cout << endl << duplicates << endl;
+        calculateDuplicates();
+        return duplicates;
     }
     
     // check if the list is empty
     bool isEmpty() {
-        
-        //TBC
+        return wordlist->empty();
     }
     
     // Empty the list
     void makeEmpty() {
-        // TO BE COMPLETED
+        wordlist->clear();
+        n = 0;
+        duplicates = 0;
+    }
+    
+    void calculateDuplicates()
+    {
+        duplicates = 0;
+        list<string>::iterator i;
+        list<string>::iterator j;
         
+        for (i = wordlist->begin(); i != wordlist->end(); ++i)
+        {
+            int count = 0;
+            for (j = i; j != wordlist->end(); ++j)
+            {
+                if (count == 0)
+                {
+                    count = 1;
+                    continue;
+                }
+                string a = *i;
+                string b = *j;
+                if (a.compare(b) == 0)
+                {
+                    duplicates++;
+                }
+                break;
+            }
+        }
     }
     
 private:
